76150739399acbcf73f693548abb82dfed7d1dcc
Eric Rizzi <eric.rizzi@gmail.com>
2024-04-02 18:02:18 -0500

    Done.... just needs to be tested

--- src/turbozzle/puzzles/puzzle_7.py
+++ src/turbozzle/puzzles/puzzle_7.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
-import time
-
 from turbozzle.utils.movement_wrapper import forward, init_puzzle, left, on_red, right, wait_until_exit
 
 
 def solve_puzzle_7() -> None:
-    for i in range(4):
-        for i in range(5):
-            forward()
-        left(180)
-        for i in range(5):
-            forward()
-        left(90)
+    "YOUR CODE HERE"
 
 
 init_puzzle("levels/puzzle_7.png", x=0, y=0, speed=0)
 solve_puzzle_7()
-time.sleep(10)
 
 wait_until_exit()
