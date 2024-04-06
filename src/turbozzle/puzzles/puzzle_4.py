70af987b68041f7919adc6fed5f0391818c8fd06
Eric Rizzi <eric.rizzi@gmail.com>
2024-04-06 12:23:28 -0500

    Simplified level 4 a bit

--- src/turbozzle/puzzles/puzzle_4.py
+++ src/turbozzle/puzzles/puzzle_4.py
@@ -4,7 +4,6 @@ from turbozzle.utils.movement_wrapper import forward, init_puzzle, left, wait_un
 
 def solve_puzzle_4() -> None:
 
-    forward()
     for i in range(5):
         for i in range(2):
             forward()
@@ -17,7 +16,7 @@ def solve_puzzle_4() -> None:
         left(90)
 
 
-init_puzzle("levels/puzzle_4.png", x=-300, y=-200, speed=0)
+init_puzzle("levels/puzzle_4.png", x=-250, y=-200, speed=0)
 solve_puzzle_4()
 
 wait_until_exit()
