2cbd78e665788a5d95fa13fe4972e1f10a0d35a1
Eric Rizzi <eric.rizzi@gmail.com>
2024-06-22 13:11:58 -0500

    Rotate puzzle_05 so need to start pointing up

--- src/turbozzle/puzzles/puzzle_05.py
+++ src/turbozzle/puzzles/puzzle_05.py
@@ -3,6 +3,7 @@ from turbozzle.utils.movement_wrapper import forward, init_puzzle, left, on_purp
 
 
 def solve_puzzle_05() -> None:
+    left(90)
     while True:
         forward()
         if on_red():
@@ -11,7 +12,7 @@ def solve_puzzle_05() -> None:
             left(90)
 
 
-init_puzzle("levels/puzzle_05.png", x=-250, y=0, speed=2)
+init_puzzle("levels/puzzle_05.png", x=0, y=-250, speed=0)
 solve_puzzle_05()
 
 wait_until_exit()
